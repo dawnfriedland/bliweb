26619660b6c482aa38546e61a14b41b10b8e17df
dawnfriedland <dawn@barklouder.com>
2012-08-24 23:24:07 -0700

    adding configs for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,21 @@ BliWeb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.google_analytics_page_code = "UA-34325069-1" 
+  
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.perform_deliveries = true
+   config.action_mailer.delivery_method = :smtp
+  
+   config.action_mailer.smtp_settings = {
+     address:         "smtp.gmail.com",
+     port:            "587",
+     domain:          "barklouder.com",
+     authentication:  :login,
+     outgoing_address: "info@barklouder.com",
+     user_name:       'info@barklouder.com',
+     password:        'Tiptoethroughtulips'
+   }
+  
 end
